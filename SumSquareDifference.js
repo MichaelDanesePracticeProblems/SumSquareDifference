e1c2a696a2e98e409cd455339fc2c4e7bbc4ba75
Micahel Danese <JoshDanese@gmail.com>
2020-05-03 10:01:42 -0400

    Added comments and added small changes to how input is handled

--- SumSquareDifference.js
+++ SumSquareDifference.js
@@ -5,6 +5,10 @@ const FindTheDifference = (min = 1, max = 100) => {
       squareOfSum += i;
     }
     squareOfSum *= squareOfSum;
+    Output(sumOfSquares, squareOfSum, min, max);
+  }//Finds the difference between the sum of all the numbers between the max and min, squared, and the sum of all squared numbers between max and min.
+
+  const Output = (sumOfSquares, squareOfSum, min, max) => {
     var output = document.getElementById("output");
     var div = document.createElement("div");
     div.innerHTML = "Solving the SquareSumDifference problem from " + min + " to " + max + "... <br>" ;
@@ -12,10 +16,14 @@ const FindTheDifference = (min = 1, max = 100) => {
     div.innerHTML += "The sum of the squares is: " + sumOfSquares + " <br>";
     div.innerHTML += "The difference is: " + (squareOfSum - sumOfSquares) + "<br>";
     output.appendChild(div);
-  }
+}//Handles the output of the HTML File
 
 const ParseInput = () => {
     var min = parseInt(document.getElementById("min").value), max = parseInt(document.getElementById("max").value);
-    console.log(min + " " + max); 
-    FindTheDifference(min, max); 
-}
+    if(min < max){
+        FindTheDifference(min, max); 
+    }
+    else{
+        alert("Min must be smaller than Max");
+    }
+}//Takes in the input from the HTML file from the max and min textarea.
